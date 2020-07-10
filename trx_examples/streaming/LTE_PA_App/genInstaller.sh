b23ccbf58be9e2bfd2f14e4528f922e6ba245d60
SrikanthPagadarai <srikanth.pagadarai@analog.com>
2020-07-10 17:59:24 -0400

    Added bash script to generate installer from commandline.
    Signed-off-by: SrikanthPagadarai <srikanth.pagadarai@analog.com>

---
+++ trx_examples/streaming/LTE_PA_App/genInstaller.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/local/MATLAB/R2020a/bin/matlab -nodisplay -nodesktop -nosplash -r 'genLTEPAInstaller'
---
+++ trx_examples/streaming/LTE_PA_App/genLTEPAInstaller.m
@@ -0,0 +1,27 @@
+cd(fileparts((mfilename('fullpath'))));
+p = pwd;
+
+version = '19.2.1';
+ml = ver('MATLAB');
+ml = ml.Release(2:end-1);
+
+tmpl_name = 'LTEPA.tmpl';
+fid  = fopen(tmpl_name,'r');
+f=fread(fid,'*char')';
+fclose(fid);
+
+f = strrep(f,'__REPO-ROOT__',p);
+f = strrep(f,'__VERSION__',version);
+f = strrep(f,'__ML-RELEASE__',ml);
+
+tmpl_name_parts = strsplit(tmpl_name, '.');
+app_name = tmpl_name_parts{1};
+projectfile = strcat(app_name, '.prj');
+fid  = fopen(projectfile,'w');
+for ii=1:size(f,2)
+    fprintf(fid,'%s',f(ii));
+end
+fclose(fid);
+
+deploytool('-package', projectfile);
+delete(projectfile);
