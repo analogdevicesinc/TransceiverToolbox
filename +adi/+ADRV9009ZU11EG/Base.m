da152b3c36f762fe2090fa7bb360e1a4e1952a3b
Travis F. Collins <travis.collins@analog.com>
2020-09-23 12:28:26 -0700

    Add Basic ADRV9009ZU11EG API
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

---
+++ +adi/+ADRV9009ZU11EG/Base.m
@@ -0,0 +1,46 @@
+classdef (Abstract, Hidden = true) Base < adi.ADRV9009.Base
+    
+    %adi.ADRV9009ZU11EG.Base Class
+    %   This class contains shared parameters and methods between TX and RX
+    %   classes
+    
+    properties
+        %CenterFrequency Center Frequency Chip B
+        %   RF center frequency, specified in Hz as a scalar. The
+        %   default is 2.4e9.  This property is tunable.
+        CenterFrequencyChipB = 2.4e9;
+    end
+    
+    methods
+        % Check CenterFrequencyChipB
+        function set.CenterFrequencyChipB(obj, value)
+            validateattributes( value, { 'double','single' }, ...
+                { 'real', 'positive','scalar', 'finite', 'nonnan', 'nonempty','integer','>=',70e6,'<=',6e9}, ...
+                '', 'CenterFrequencyChipB');
+            obj.CenterFrequencyChipB = value;
+            if obj.ConnectedToDevice
+                id = 'altvoltage0';
+                obj.setAttributeLongLong(id,'frequency',value,true);
+            end
+        end
+    end
+    
+    %% API Functions
+    methods (Hidden, Access = protected)
+               
+        function icon = getIconImpl(obj)
+            icon = sprintf(['ADRV9009ZU11EG ',obj.Type]);
+        end
+        
+    end
+    
+    %% External Dependency Methods
+    methods (Hidden, Static)
+        
+        function bName = getDescriptiveName(~)
+            bName = 'ADRV9009ZU11EG';
+        end
+        
+    end
+end
+
