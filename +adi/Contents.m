86bd8914bfac435b3cf2aef1111d18ddd04bf49e
Travis F. Collins <travis.collins@analog.com>
2020-09-25 18:18:05 -0700

    Update contents table for new parts
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- +adi/Contents.m
+++ +adi/Contents.m
@@ -9,6 +9,7 @@
 %   <a href="matlab:help adi.AD9363            ">AD9363</a>         - Transceiver
 %   <a href="matlab:help adi.AD9364            ">AD9364</a>         - Transceiver
 %   <a href="matlab:help adi.AD9371            ">AD9371</a>         - Transceiver
+%   <a href="matlab:help adi.ADRV9002          ">ADRV9002</a>       - Transceiver
 %   <a href="matlab:help adi.ADRV9009          ">ADRV9009</a>       - Transceiver
 %
 % Boards and Platforms
@@ -21,5 +22,7 @@
 %   <a href="matlab:help adi.ADRV9361Z7035     ">ADRV9361-Z7035</a> - SOM based on the AD9361 with Zynq-Z7035
 %   <a href="matlab:help adi.ADRV9364Z7020     ">ADRV9364-Z7020</a> - SOM based on the AD9364 with Zynq-Z7020
 %   <a href="matlab:help adi.ADRV9371          ">ADRV9371</a>       - FMC development board based on the AD9371
+%   <a href="matlab:help adi.ADRV9002          ">ADRV9002</a>       - FMC development board based on the ADRV9002
 %   <a href="matlab:help adi.ADRV9009          ">ADRV9009</a>       - FMC development board based on the ADRV9009
+%   <a href="matlab:help adi.ADRV9009ZU11EG    ">ADRV9009-ZU11EG</a>- SOM based on the ADRV9009 with Zynq-Ultrascale 11EG
 
