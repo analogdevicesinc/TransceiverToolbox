5b70c1921efb95d94ef09be8cf0db5ef22301c20
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2020-10-09 19:04:52 -0400

    Added doc generation files for system object properties
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- CI/doc/genhtml.m
+++ CI/doc/genhtml.m
@@ -1,3 +1,23 @@
+mfiledir = '..\..\+adi\';
+docdir = '..\..\doc\';
+parts = {'AD9144'; 'AD9361'; 'AD9363'; 'AD9364'; ...
+    'AD9371'; 'AD9680'; 'ADRV9009'; 'ADRV9361Z7035'; ...
+    'ADRV9364Z7020'; 'ADRV9371'; 'DAQ2'; 'FMComms2'; ...
+    'FMComms3'; 'FMComms4'; 'PackRF'; 'Pluto'};
+trx_files = {'Tx','Rx'};
+for ii = 1:numel(parts)
+    for jj = 1:numel(trx_files)
+        dotmfilename = strcat(mfiledir, '+', parts{ii}, '\', trx_files{jj}, '.m');
+        if isfile(dotmfilename)
+            htmlfilename = strcat(docdir, parts{ii}, '_', trx_files{jj}, '.html');
+            html = customDoc(dotmfilename);
+            dlmwrite(htmlfilename, html, 'delimiter', '');
+            disp(htmlfilename);
+        end
+    end
+end
+publish('SysObjsProps.m','outputDir','..\..\doc');
+
 [filepath,name,ext] = fileparts(mfilename('fullpath'));
 cd(filepath);
 files = dir(filepath);
