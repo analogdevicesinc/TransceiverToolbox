bbc6999ef95c7b8536a5a5c2b6a64cff30f529eb
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2020-10-12 11:15:39 -0400

    New homepage. Under construction.
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

---
+++ CI/doc/ADITTBHome.m
@@ -0,0 +1,16 @@
+%% ANALOG DEVICES INC. TRANSCEIVER TOOLBOX SUPPORT PACKAGE
+% Analog Devices Inc. Transceiver Toolbox Support Package for Analog 
+% Devices transceivers enable the user in designing and prototyping 
+% practical wireless systems. Using MATLAB system objects provided with 
+% this support package, you can use ADI transceiver products to either 
+% capture streaming RF data and apply signal processing algorithms in 
+% MATLAB or utilize the provided HDL infrastructre to develop targetable 
+% designs that process RF data on the fly, thereby facilitating 
+% the design, development and testing of modem designs under real-world 
+% conditions. 
+% 
+% <SupportedHardware.html Installation and Supported Hardware>
+% 
+% <Examples.html Featured Examples>
+% 
+% <Support.html Support>
