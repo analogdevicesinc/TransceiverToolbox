254fa4a9866d03f2043d7f225840b46301ed22d7
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2020-10-30 16:44:23 -0400

    Added html pages generated from system objects.
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- CI/doc/ADITTBHome.m
+++ CI/doc/ADITTBHome.m
@@ -9,8 +9,49 @@
 % the design, development and testing of modem designs under real-world 
 % conditions. 
 % 
-% <SupportedHardware.html Installation and Supported Hardware>
+% <libiioCore.html libiio Core Concepts>
+%{
+Refer to existing documentation as much as possible. Only provide an 
+overview from a MATLAB perspective.
+%}
+% 
+% <CodeExamples.html Code Examples>
+%{
+Describe API in a unified manner to appeal to all users regardless of 
+whether Pluto, AD936x, AD937x, ADRV900x is being used to run code examples.
+%}
+% 
+% <Examples.html Targeting>
+%{
+What does HDL coder do?
+IQ in IQ out
+Valid in Valid out
+Discussion on targeting from a non-AD9361 perspective since MATLAB
+documentation has AD9361 perspective on HDL. Point to wiki page on 
+Transceiver toolbox. But, update it first. 
+%}
 % 
 % <Examples.html Featured Examples>
+%{
+Streaming Examples
+Targeting Examples (Workflow from MATLAB Algorithm implementation to FPGA 
+targeting) - point to the donut diagram 
+Show examples the way MATLAB doc shows - demos.xml, similarly, see if there is an apps.xml 
+published examples, 
+%}
 % 
-% <Support.html Support>
+% <AdvTopics.html Advanced(?) Topics>
+% 
+%{
+State Management 
+- When you change an attribute, when does it take effect?
+- What happens when you run the system object operator?
+Attributes
+Debug Attributes
+Fast lock/frequency hopping
+...
+...
+AGC Tuning (For AD936x)
+%}
+%
+% <ForDevs.html For Developers>
