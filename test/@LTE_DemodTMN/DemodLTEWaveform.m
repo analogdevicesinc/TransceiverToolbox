6b9eea6d5032ac26659acfb74d2f99a979209764
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2021-02-24 15:13:56 -0500

    LTE test harness: Removed reference to app in LTE_DemodTMN class folder
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- test/@LTE_DemodTMN/DemodLTEWaveform.m
+++ test/@LTE_DemodTMN/DemodLTEWaveform.m
@@ -30,69 +30,65 @@ function logEVM = DemodLTEWaveform(eNodeBInput, etm)
     count = 1;
     fprintf('\n');
     for i=0:nSubframes-1
-        %{
-       app.SummaryTable1_Data{1} = logEVM.CyclicPrefix;
-       app.SummaryTable1_Data{2} = logEVM.NCellID;
-        %}
         fprintf('*');
-       % fprintf('Processing Subframe #%d\n', i);        
+        % fprintf('Processing Subframe #%d\n', i);        
 
-       [EqGridStruct, EVMStruct, evm, allocatedSymbols, rxSymbols, ...
+        [EqGridStruct, EVMStruct, evm, allocatedSymbols, rxSymbols, ...
            refSymbols, pdsch_ind, etm] = ...
            LTE_DemodTMN.EVMSubframe(i, nSubframes, etm, allPRBSet, ...
            refGrid, rxGridLow, rxGridHigh, HestLow, HestHigh);
-       if (etm.CellRefP ~= 1) && (etm.CellRefP ~= 2) && (etm.CellRefP ~= 4)
+        if (etm.CellRefP ~= 1) && (etm.CellRefP ~= 2) && (etm.CellRefP ~= 4)
            error('Test stopped. RF loopback cable likely disconnected. For the demodulated parameter field CellRefP, the value (%d) is not one of the set (1, 2, 4).\n',...
                etm.CellRefP);
-       end
-       logEVM.EqGridStruct = EqGridStruct;
-       logEVM.DemodSyms = ...
+        end
+        logEVM.EqGridStruct = EqGridStruct;
+        logEVM.DemodSyms = ...
            struct('Rec', rxSymbols, 'Ref', refSymbols);
 
-       if isfield(EVMStruct, 'PBCH')
+        if isfield(EVMStruct, 'PBCH')
            logEVM.evmRMSCh.PBCH = 100*EVMStruct.PBCH;
-       end               
-       if isfield(EVMStruct, 'PCFICH')
+        end               
+        if isfield(EVMStruct, 'PCFICH')
            logEVM.evmRMSCh.PCFICH = 100*EVMStruct.PCFICH;
-       end
-       if isfield(EVMStruct, 'PHICH')
+        end
+        if isfield(EVMStruct, 'PHICH')
            logEVM.evmRMSCh.PHICH = 100*EVMStruct.PHICH;
-       end
-       if isfield(EVMStruct, 'PDCCH')
+        end
+        if isfield(EVMStruct, 'PDCCH')
            logEVM.evmRMSCh.PDCCH = 100*EVMStruct.PDCCH;
-       end
-       if isfield(EVMStruct, 'RS')
+        end
+        if isfield(EVMStruct, 'RS')
            logEVM.evmRMSCh.RS = 100*EVMStruct.RS;
-       end
-       if isfield(EVMStruct, 'PSS')
+        end
+        if isfield(EVMStruct, 'PSS')
            logEVM.evmRMSCh.PSS = 100*EVMStruct.PSS;
-       end
-       if isfield(EVMStruct, 'SSS')
+        end
+        if isfield(EVMStruct, 'SSS')
            logEVM.evmRMSCh.SSS = 100*EVMStruct.SSS;
-       end
+        end
 
-       [SymbEVM, ScEVM, RbEVM, frameLowEVM, frameHighEVM, frameEVM, etm,...
+        [SymbEVM, ScEVM, RbEVM, frameLowEVM, frameHighEVM, frameEVM, etm,...
            count, ~] = ...
            LTE_DemodTMN.DemodSymbs(i, pdsch_ind, nFrames, ...
            count, alg, etm, evm, ...
            allocatedSymbols, frameEVM, nSubframes);
-       SymbEVM.evmSymbolRMS(1) = SymbEVM.evmSymbolRMS(2);
-       SymbEVM.evmSymbolPeak(1) = SymbEVM.evmSymbolPeak(2);
-       logEVM.evmSC = ...
+        SymbEVM.evmSymbolRMS(1) = SymbEVM.evmSymbolRMS(2);
+        SymbEVM.evmSymbolPeak(1) = SymbEVM.evmSymbolPeak(2);
+        logEVM.evmSC = ...
            struct('RMS', ScEVM.evmSubcarrierRMS, 'Peak', ScEVM.evmSubcarrierPeak, ...
            'EVMGrid', ScEVM.evmGrid); 
-       PDSCHevm_temp = ScEVM.evmGrid(:);
-       logEVM.evmRMSCh.PDSCH = mean(PDSCHevm_temp(PDSCHevm_temp~=0));
-       logEVM.evmRB = ...
+        PDSCHevm_temp = ScEVM.evmGrid(:);
+        logEVM.evmRMSCh.PDSCH = mean(PDSCHevm_temp(PDSCHevm_temp~=0));
+        logEVM.evmRB = ...
            struct('RMS', RbEVM.evmRBRMS, 'Peak', RbEVM.evmRBPeak);  
-       logEVM.evmSymbol = ...
+        logEVM.evmSymbol = ...
            struct('RMS', SymbEVM.evmSymbolRMS, 'Peak', SymbEVM.evmSymbolPeak);
 
-       if (mod(i, 10)==9 || (nFrames==0 && i==nSubframes-1))                       
+        if (mod(i, 10)==9 || (nFrames==0 && i==nSubframes-1))                       
            logEVM.FrameEVM = ...
                struct('Low', frameLowEVM, ...
                'High', frameHighEVM, 'Overall', frameEVM);                                       
-       end                    
+        end                    
     end
     % Final Mean EVM across all frames
     logEVM.FinalEVM = lteEVM(cat(1, frameEVM(:).EV));      
