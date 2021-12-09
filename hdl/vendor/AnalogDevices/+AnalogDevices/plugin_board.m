563cdfa2c2133f831ae4b275fb75d62bb4846de2
Travis F. Collins <travis.collins@analog.com>
2021-12-08 16:09:49 -0800

    Update generic plugin_rd to include US

--- hdl/vendor/AnalogDevices/+AnalogDevices/plugin_board.m
+++ hdl/vendor/AnalogDevices/+AnalogDevices/plugin_board.m
@@ -14,26 +14,31 @@ hB.BoardName    = sprintf('AnalogDevices %s %s', upper(pname), upper(board));
 
 % FPGA Device
 hB.FPGAVendor   = 'Xilinx';
-hB.FPGAFamily   = 'Zynq';
 
 % Determine the device based on the board
 switch(upper(board))
+	case 'ZCU102'
+		hB.FPGADevice   = sprintf('xc%s', 'zu9eg-ffvb1156-2-e');
+		hB.FPGAPackage  = '';
+		hB.FPGASpeed    = '';
+		hB.FPGAFamily   = 'Zynq UltraScale+';
 	case 'ZC706'
 		hB.FPGADevice   = sprintf('xc7%s', 'z045');
 		hB.FPGAPackage  = 'ffg900';
 		hB.FPGASpeed    = '-2';
+		hB.FPGAFamily   = 'Zynq';
 	case 'ZC702'
 		hB.FPGADevice   = sprintf('xc7%s', 'z020');
 		hB.FPGAPackage  = 'clg484';
 		hB.FPGASpeed    = '-1';	
+		hB.FPGAFamily   = 'Zynq';
 	case 'ZED'
 		hB.FPGADevice   = sprintf('xc7%s', 'z020');
 		hB.FPGAPackage  = 'clg484';
 		hB.FPGASpeed    = '-1';	
+		hB.FPGAFamily   = 'Zynq';
 	otherwise
-		hB.FPGADevice   = sprintf('xc7%s', 'z045');
-		hB.FPGAPackage  = 'ffg900';
-		hB.FPGASpeed    = '-2';
+		error(sprintf('Unknown board: %s',board));
 end
 
 % Tool Info
