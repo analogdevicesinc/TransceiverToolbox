5d06cbd46480dab8d78400eef37d0d2a593b0c34
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2022-01-20 11:02:14 -0500

    Updated plugin_rd common file and deleted old copies
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- hdl/vendor/AnalogDevices/+AnalogDevices/get_memory_axi_interface_info.m
+++ hdl/vendor/AnalogDevices/+AnalogDevices/get_memory_axi_interface_info.m
@@ -2,34 +2,6 @@ function out = get_memory_axi_interface_info(fpga,project)
 
 
 switch project
-    case 'fmcomms2'
-        switch fpga
-            case {'ZC702','ZC706'}
-                InterfaceConnection = 'axi_cpu_interconnect/M09_AXI';
-                BaseAddress = '0x43C00000';
-                MasterAddressSpace = 'sys_ps7/Data';
-            case 'ZED'
-                InterfaceConnection = 'axi_cpu_interconnect/M11_AXI';
-                BaseAddress = '0x43C00000';
-                MasterAddressSpace = 'sys_ps7/Data';
-            otherwise
-                error(sprintf('Unknown Project FPGA %s/%s',project,fpga)); %#ok<*SPERR>
-        end
-    case 'adrv9371x'
-        switch fpga
-            case {'ZC706'}
-                hRD.addAXI4SlaveInterface( ...
-                    'InterfaceConnection', 'axi_cpu_interconnect/M21_AXI', ... % ADC DMA BUS
-                    'BaseAddress',         '0x50000000', ...
-                    'MasterAddressSpace',  'sys_ps7/Data');
-            case {'ZCU102'}
-                hRD.addAXI4SlaveInterface( ...
-                    'InterfaceConnection', 'axi_cpu_interconnect/M16_AXI', ... % ADC DMA BUS
-                    'BaseAddress',         '0x9D000000', ...
-                    'MasterAddressSpace',  'sys_ps8/Data');                
-            otherwise
-                error(sprintf('Unknown Project FPGA %s/%s',project,fpga)); %#ok<*SPERR>
-        end
     case 'adrv9002'
         switch fpga
             case {'ZCU102'}
@@ -48,6 +20,63 @@ switch project
             otherwise
                 error(sprintf('Unknown Project FPGA %s/%s',project,fpga)); %#ok<*SPERR>
         end
+    case 'adrv9361z7035'
+        switch lower(fpga)
+            case 'ccfmc_lvds'
+                InterfaceConnection = 'axi_cpu_interconnect/M12_AXI';
+                BaseAddress = '0x43C00000';
+                MasterAddressSpace = 'sys_ps7/Data';
+            case {'ccbob_cmos','ccbob_lvds'}
+                InterfaceConnection = 'axi_cpu_interconnect/M07_AXI';
+                BaseAddress = '0x43C00000';
+                MasterAddressSpace = 'sys_ps7/Data';
+            case {'ccbox_lvds','ccpackrf_lvds'}
+                InterfaceConnection = 'axi_cpu_interconnect/M06_AXI';
+                BaseAddress = '0x43C00000';
+                MasterAddressSpace = 'sys_ps7/Data';
+            otherwise
+                error(sprintf('Unknown Project FPGA %s/%s',project,fpga)); %#ok<*SPERR>
+        end
+    case 'adrv9364z7020'
+        InterfaceConnection = 'axi_cpu_interconnect/M06_AXI';
+        BaseAddress = '0x43C00000';
+        MasterAddressSpace = 'sys_ps7/Data';
+    case 'adrv9371x'
+        switch fpga
+            case {'ZC706'}
+                hRD.addAXI4SlaveInterface( ...
+                    'InterfaceConnection', 'axi_cpu_interconnect/M21_AXI', ... % ADC DMA BUS
+                    'BaseAddress',         '0x50000000', ...
+                    'MasterAddressSpace',  'sys_ps7/Data');
+            case {'ZCU102'}
+                hRD.addAXI4SlaveInterface( ...
+                    'InterfaceConnection', 'axi_cpu_interconnect/M16_AXI', ... % ADC DMA BUS
+                    'BaseAddress',         '0x9D000000', ...
+                    'MasterAddressSpace',  'sys_ps8/Data');                
+            otherwise
+                error(sprintf('Unknown Project FPGA %s/%s',project,fpga)); %#ok<*SPERR>
+        end
+    case 'fmcomms2'
+        switch fpga
+            case {'ZC702','ZC706'}
+                InterfaceConnection = 'axi_cpu_interconnect/M09_AXI';
+                BaseAddress = '0x43C00000';
+                MasterAddressSpace = 'sys_ps7/Data';
+            case 'ZED'
+                InterfaceConnection = 'axi_cpu_interconnect/M11_AXI';
+                BaseAddress = '0x43C00000';
+                MasterAddressSpace = 'sys_ps7/Data';
+            otherwise
+                error(sprintf('Unknown Project FPGA %s/%s',project,fpga)); %#ok<*SPERR>
+        end
+    case 'fmcomms5'
+        InterfaceConnection = 'axi_cpu_interconnect/M10_AXI';
+        BaseAddress = '0x43C00000';
+        MasterAddressSpace = 'sys_ps7/Data';
+    case 'pluto'
+        InterfaceConnection = 'axi_cpu_interconnect/M05_AXI';
+        BaseAddress = '0x43C00000';
+        MasterAddressSpace = 'sys_ps7/Data';
     otherwise
         error(sprintf('Unknown Project %s',project)); %#ok<*SPERR>
 end
--- hdl/vendor/AnalogDevices/+AnalogDevices/plugin_board.m
+++ hdl/vendor/AnalogDevices/+AnalogDevices/plugin_board.m
@@ -16,29 +16,117 @@ hB.BoardName    = sprintf('AnalogDevices %s %s', upper(pname), upper(board));
 hB.FPGAVendor   = 'Xilinx';
 
 % Determine the device based on the board
-switch(upper(board))
-	case 'ZCU102'
-		hB.FPGADevice   = sprintf('xc%s', 'zu9eg-ffvb1156-2-e');
-		hB.FPGAPackage  = '';
-		hB.FPGASpeed    = '';
-		hB.FPGAFamily   = 'Zynq UltraScale+';
-	case 'ZC706'
-		hB.FPGADevice   = sprintf('xc7%s', 'z045');
-		hB.FPGAPackage  = 'ffg900';
-		hB.FPGASpeed    = '-2';
-		hB.FPGAFamily   = 'Zynq';
-	case 'ZC702'
-		hB.FPGADevice   = sprintf('xc7%s', 'z020');
-		hB.FPGAPackage  = 'clg484';
-		hB.FPGASpeed    = '-1';	
-		hB.FPGAFamily   = 'Zynq';
-	case 'ZED'
-		hB.FPGADevice   = sprintf('xc7%s', 'z020');
-		hB.FPGAPackage  = 'clg484';
-		hB.FPGASpeed    = '-1';	
-		hB.FPGAFamily   = 'Zynq';
-	otherwise
-		error(sprintf('Unknown board: %s',board));
+switch lower(project)
+    case 'adrv9002'
+        switch(upper(board))
+            % 	case 'ZC706'
+            % 		hB.FPGADevice   = sprintf('xc7%s', 'z045');
+            % 		hB.FPGAPackage  = 'ffg900';
+            % 		hB.FPGASpeed    = '-2';
+            case 'ZCU102'
+                hB.FPGADevice   = sprintf('xc%s', 'zu9eg-ffvb1156-2-e');
+                hB.FPGAPackage  = '';
+                hB.FPGASpeed    = '';
+                hB.FPGAFamily   = 'Zynq UltraScale+';
+            otherwise
+                hB.FPGADevice   = sprintf('xc%s', 'zu9eg-ffvb1156-2-e');
+                hB.FPGAPackage  = '';
+                hB.FPGASpeed    = '';
+                hB.FPGAFamily   = 'Zynq';
+        end
+    case 'adrv9009'
+        switch(upper(board))
+        % 	case 'ZC706'
+        % 		hB.FPGADevice   = sprintf('xc7%s', 'z045');
+        % 		hB.FPGAPackage  = 'ffg900';
+        % 		hB.FPGASpeed    = '-2';
+            case 'ZCU102'
+                hB.FPGADevice   = sprintf('xc%s', 'zu9eg-ffvb1156-2-e');
+                hB.FPGAPackage  = '';
+                hB.FPGASpeed    = '';
+                hB.FPGAFamily   = 'Zynq UltraScale+';
+            otherwise
+                hB.FPGADevice   = sprintf('xc%s', 'zu9eg-ffvb1156-2-e');
+                hB.FPGAPackage  = '';
+                hB.FPGASpeed    = '';
+                hB.FPGAFamily   = 'Zynq';
+        end
+    case 'adrv9361z7035'
+        hB.FPGADevice   = sprintf('xc7%s', 'z035i');
+        hB.FPGAPackage  = 'fbg676';
+        hB.FPGASpeed    = '-2L';
+        hB.FPGAFamily   = 'Zynq';
+    case 'adrv9364z7020'
+        hB.FPGADevice   = sprintf('xc7%s', 'z020');
+        hB.FPGAPackage  = 'clg400';
+        hB.FPGASpeed    = '-1';
+        hB.FPGAFamily   = 'Zynq';
+    case 'fmcomms2'
+        switch(upper(board))
+            case 'ZCU102'
+                hB.FPGADevice   = sprintf('xc%s', 'zu9eg-ffvb1156-2-e');
+                hB.FPGAPackage  = '';
+                hB.FPGASpeed    = '';
+                hB.FPGAFamily   = 'Zynq UltraScale+';
+            case 'ZC706'
+                hB.FPGADevice   = sprintf('xc7%s', 'z045');
+                hB.FPGAPackage  = 'ffg900';
+                hB.FPGASpeed    = '-2';
+                hB.FPGAFamily   = 'Zynq';
+            case 'ZC702'
+                hB.FPGADevice   = sprintf('xc7%s', 'z020');
+                hB.FPGAPackage  = 'clg484';
+                hB.FPGASpeed    = '-1';	
+                hB.FPGAFamily   = 'Zynq';
+            case 'ZED'
+                hB.FPGADevice   = sprintf('xc7%s', 'z020');
+                hB.FPGAPackage  = 'clg484';
+                hB.FPGASpeed    = '-1';	
+                hB.FPGAFamily   = 'Zynq';
+            otherwise
+                error(sprintf('Unknown board: %s',board));
+        end
+    case 'fmcomms5'
+        switch(upper(board))
+            case 'ZC706'
+                hB.FPGADevice   = sprintf('xc7%s', 'z045');
+                hB.FPGAPackage  = 'ffg900';
+                hB.FPGASpeed    = '-2';
+                hB.FPGAFamily   = 'Zynq';
+            case 'ZC702'
+                hB.FPGADevice   = sprintf('xc7%s', 'z020');
+                hB.FPGAPackage  = 'clg484';
+                hB.FPGASpeed    = '-1';	
+                hB.FPGAFamily   = 'Zynq';
+            otherwise
+                hB.FPGADevice   = sprintf('xc7%s', 'z045');
+                hB.FPGAPackage  = 'ffg900';
+                hB.FPGASpeed    = '-2';
+                hB.FPGAFamily   = 'Zynq';
+        end
+    case 'pluto'
+        hB.FPGAFamily   = 'Zynq';
+        hB.FPGADevice   = sprintf('xc7%s', 'z010');
+        hB.FPGAPackage  = 'clg225';
+        hB.FPGASpeed    = '-1';	
+    case 'adrv9371x'
+        switch(upper(board))
+            case 'ZC706'
+                hB.FPGAFamily   = 'Zynq';
+                hB.FPGADevice   = sprintf('xc7%s', 'z045');
+                hB.FPGAPackage  = 'ffg900';
+                hB.FPGASpeed    = '-2';
+            case 'ZCU102'
+                hB.FPGAFamily   = 'Zynq UltraScale+';
+                hB.FPGADevice   = sprintf('xc%s', 'zu9eg-ffvb1156-2-e');
+                hB.FPGAPackage  = '';
+                hB.FPGASpeed    = '';
+            otherwise
+                hB.FPGAFamily   = 'Zynq UltraScale+';
+                hB.FPGADevice   = sprintf('xc%s', 'zu9eg-ffvb1156-2-e');
+                hB.FPGAPackage  = '';
+                hB.FPGASpeed    = '';
+        end
 end
 
 % Tool Info
