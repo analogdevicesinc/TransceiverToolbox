bf32004fc0065412bd617803bd527047d76f23e9
Srikanth Pagadarai <srikanth.pagadarai@analog.com>
2022-02-22 15:14:57 -0500

    Customization of system_project_rxtx.tcl for pluto
    Signed-off-by: Srikanth Pagadarai <srikanth.pagadarai@analog.com>

--- CI/scripts_v2/system_project_rxtx.tcl
+++ CI/scripts_v2/system_project_rxtx.tcl
@@ -1,7 +1,12 @@
 set start_dir [pwd]
 puts "Starting Transceiver Toolbox HDL build"
-cd projects/$project/$carrier
-source ../../scripts/adi_make.tcl
+if {$project == "pluto"} {
+    cd projects/$project/
+    source ../scripts/adi_make.tcl
+} else {
+    cd projects/$project/$carrier
+    source ../../scripts/adi_make.tcl
+}
 adi_make::lib all
 
 set ::env(SKIP_SYNTHESIS) 1
@@ -10,7 +15,11 @@ set ::env(MATLAB) 1
 source ./system_project.tcl
 
 # Update block design to make room for new IP
-source ../../scripts/matlab_processors.tcl
+if {$project == "pluto"} {
+    source ../scripts/matlab_processors.tcl
+} else {
+    source ../../scripts/matlab_processors.tcl
+}
 preprocess_bd $project $carrier $ref_design
 
 regenerate_bd_layout
