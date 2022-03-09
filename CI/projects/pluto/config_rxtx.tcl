ea7ca2461b4cbccb75761b059fe781a01826b1f4
Travis F. Collins <travis.collins@analog.com>
2022-03-09 10:47:28 -0800

    Remove old CI hdl source
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- CI/projects/pluto/config_rxtx.tcl
+++
@@ -1,7 +0,0 @@
-set ad_hdl_dir    	[pwd]
-set proj_dir		$ad_hdl_dir/projects/pluto
-
-source $proj_dir/config_prj.tcl
-source $ad_hdl_dir/projects/pluto/config_rxtx_board.tcl
-
-regenerate_bd_layout
