3a239942c299396108b07861b1b21a67d7471451
Travis F. Collins <travis.collins@analog.com>
2022-08-03 16:57:12 -0600

    Update ADRV9002 profiles
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- trx_examples/streaming/adrv9002/ADRV9002_DMA_Example.m
+++ trx_examples/streaming/adrv9002/ADRV9002_DMA_Example.m
@@ -1,25 +1,31 @@
 clear all;
 
+uri = 'ip:analog';
+
 amplitude = 2^15; frequency = 0.12e6;
 swv1 = dsp.SineWave(amplitude, frequency);
 swv1.ComplexOutput = true;
 swv1.SamplesPerFrame = 1e4*10;
-swv1.SampleRate = 1.92e6;
+swv1.SampleRate = 7.68e6;
 x = swv1();
 
+%% Set profile
+rx = adi.ADRV9002.Rx('uri', uri);
+rx.EnableCustomProfile = true;
+rx.CustomProfileFileName = 'lte_5_cmos_api_48_8_7.json';
+rx.CustomStreamFileName = 'lte_5_cmos_api_48_8_7.stream';
+rx();
+
 %% Tx set up
-tx = adi.ADRV9002.Tx('uri','ip:analog');
+tx = adi.ADRV9002.Tx('uri', uri);
 tx.CenterFrequencyChannel0 = 1e9;
 tx.DataSource = 'DMA';
 tx.EnableCyclicBuffers = true;
 tx.AttenuationChannel0 = -10;
-tx.EnableCustomProfile = true;
-tx.CustomProfileFileName = fullfile('../../test/adrv9002_profiles','lte_1_4_cmos_fdd_api_29_2_9.json');
-tx.CustomStreamFileName = fullfile('../../test/adrv9002_profiles','lte_1_4_cmos_fdd_api_29_2_9.stream');
 tx(x);
 
 %% Rx set up
-rx = adi.ADRV9002.Rx('uri','ip:analog');
+rx = adi.ADRV9002.Rx('uri', uri);
 rx.CenterFrequencyChannel0 = tx.CenterFrequencyChannel0;
 rx.EnabledChannels = 1;
 rx.kernelBuffersCount = 1;
@@ -36,7 +42,7 @@ tx.release();
 rx.release();
 
 figure(1); 
-plot(0:numel(y)-1, real(y), 'r', 0:numel(y)-1, imag(y), 'b'); 
+plot(0:numel(out)-1, real(out), 'r', 0:numel(out)-1, imag(out), 'b'); 
 xlim([0 250]); 
 xlabel('sample index'); 
 grid on;
