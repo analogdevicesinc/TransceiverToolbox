639e67c39822a6170d76a80f556bdcc6c1691e09
Julia Anjanet Pineda <Julia.Pineda@analog.com>
2022-09-16 05:47:31 +0800

    Set initialize debug attr as boolean instead of long long (#120)
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>
    
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

--- +adi/+AD9361/Rx.m
+++ +adi/+AD9361/Rx.m
@@ -319,7 +319,7 @@ classdef Rx < adi.AD9361.Base & adi.AD9361.TuneAGC & ...
             if (obj.CustomAGC)
                 % Initialize hardware to reflect debug attribute changes
                 obj.WriteDebugAttributes();
-                obj.setDebugAttributeLongLong('initialize',1);           
+                obj.setDebugAttributeBool('initialize',1);           
                 obj.WriteToRegisters();
             end
             
