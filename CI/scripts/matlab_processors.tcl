6c88e24d60b5ae2712a9d8a2834c5e307c7f3194
Dirk Wischeropp <dirk.wischeropp@siemens.com>
2022-11-09 14:50:37 +0100

    AD9371: Fix reset signaling for custom IP Core
    Signed-off-by: Dirk Wischeropp <dirk.wischeropp@siemens.com>

--- CI/scripts/matlab_processors.tcl
+++ CI/scripts/matlab_processors.tcl
@@ -446,12 +446,13 @@ proc preprocess_bd {project carrier rxtx} {
                     # Add 1 extra AXI master ports to the interconnect
                     set_property -dict [list CONFIG.NUM_MI {22}] [get_bd_cells axi_cpu_interconnect]
                     #connect_bd_net -net [get_bd_nets axi_adrv9009_rx_clkgen] [get_bd_pins axi_cpu_interconnect/M13_ACLK] [get_bd_pins axi_adrv9009_rx_clkgen/clk_0]
-                    connect_bd_net [get_bd_pins sys_rstgen/interconnect_aresetn] [get_bd_pins axi_cpu_interconnect/M21_ARESETN]
 
                     if {$rxtx == "rx" || $rxtx == "rxtx"} {
+                        connect_bd_net [get_bd_pins ad9371_rx_device_clk_rstgen/peripheral_aresetn] [get_bd_pins axi_cpu_interconnect/M21_ARESETN]
                         connect_bd_net -net [get_bd_nets axi_ad9371_rx_clkgen] [get_bd_pins axi_cpu_interconnect/M21_ACLK] [get_bd_pins axi_ad9371_rx_clkgen/clk_0]
                     }
                     if {$rxtx == "tx"} {
+                        connect_bd_net [get_bd_pins ad9371_tx_device_clk_rstgen/peripheral_aresetn] [get_bd_pins axi_cpu_interconnect/M21_ARESETN]
                         connect_bd_net -net [get_bd_nets axi_ad9371_tx_clkgen] [get_bd_pins axi_cpu_interconnect/M21_ACLK] [get_bd_pins axi_ad9371_tx_clkgen/clk_0]
                     }
                 }
