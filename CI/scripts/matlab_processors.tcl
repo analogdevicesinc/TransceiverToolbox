0be79b8cf3a9b57db2224826f1c5bfb7ecf73d00
Travis F. Collins <travis.collins@analog.com>
2023-01-20 13:25:59 -0700

    Change default AXI source clks ADRV9001
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- CI/scripts/matlab_processors.tcl
+++ CI/scripts/matlab_processors.tcl
@@ -395,8 +395,15 @@ proc preprocess_bd {project carrier rxtx} {
                     # Add 1 extra AXI master ports to the interconnect
                     set_property -dict [list CONFIG.NUM_MI {7}] [get_bd_cells axi_cpu_interconnect]
                     # Connect clock and reset
-                    connect_bd_net [get_bd_pins axi_cpu_interconnect/M06_ACLK] [get_bd_pins sys_ps8/pl_clk0]
-                    connect_bd_net [get_bd_pins axi_cpu_interconnect/M06_ARESETN] [get_bd_pins sys_rstgen/peripheral_aresetn]
+                    #connect_bd_net [get_bd_pins axi_cpu_interconnect/M06_ACLK] [get_bd_pins sys_ps8/pl_clk0]
+                    #connect_bd_net [get_bd_pins axi_cpu_interconnect/M06_ARESETN] [get_bd_pins sys_rstgen/peripheral_aresetn]
+                    if {$rxtx == "rx" || $rxtx == "rxtx"} {
+                        connect_bd_net [get_bd_pins axi_cpu_interconnect/M06_ACLK] [get_bd_pins axi_adrv9001/adc_1_clk]
+                        connect_bd_net [get_bd_pins axi_cpu_interconnect/M06_ARESETN] [get_bd_pins axi_adrv9001/adc_1_rst]
+		    } else {
+                        connect_bd_net [get_bd_pins axi_cpu_interconnect/M06_ACLK] [get_bd_pins axi_adrv9001/dac_1_clk]
+                        connect_bd_net [get_bd_pins axi_cpu_interconnect/M06_ARESETN] [get_bd_pins axi_adrv9001/dac_1_rst]
+		    }
                 }                
             }
         }
