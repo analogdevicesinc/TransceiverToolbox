4c46c13d362b39d070dfc92046d0404d182e0c71
Pagadarai <Srikanth.Pagadarai@analog.com>
2023-03-23 10:01:43 -0400

    First commit for v22_1_1
    Signed-off-by: Pagadarai <Srikanth.Pagadarai@analog.com>

--- +adi/Version.m
+++ +adi/Version.m
@@ -2,10 +2,10 @@ classdef Version
     %Version
     %   BSP Version information
     properties(Constant)
-        HDL = 'hdl_2019_r2';
-        Vivado = '2019.1';
-        MATLAB = 'R2021b';
-        Release = '21.2.1';
+        HDL = 'hdl_2021_r1';
+        Vivado = '2021.1';
+        MATLAB = 'R2022a';
+        Release = '22.1.1';
     end
     properties(Dependent)
         VivadoShort
