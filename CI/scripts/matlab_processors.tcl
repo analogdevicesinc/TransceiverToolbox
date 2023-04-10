21bfd7893f4899389c35638c8dfee9ab995592bf
Pagadarai <Srikanth.Pagadarai@analog.com>
2023-04-10 17:10:57 -0400

    Updated ADRV9009 ZCU102 designs
    Signed-off-by: Pagadarai <Srikanth.Pagadarai@analog.com>

--- CI/scripts/matlab_processors.tcl
+++ CI/scripts/matlab_processors.tcl
@@ -303,13 +303,14 @@ proc preprocess_bd {project carrier rxtx} {
                     # M16 -> rx_clkgen/clk_0
 
                     # Add 1 extra AXI master ports to the interconnect
+					set_property -dict [list CONFIG.NUM_CLKS {2}] [get_bd_cells axi_cpu_interconnect]
                     set_property -dict [list CONFIG.NUM_MI {17}] [get_bd_cells axi_cpu_interconnect]
                     #connect_bd_net [get_bd_pins axi_cpu_interconnect/M16_ARESETN] [get_bd_pins sys_rstgen/peripheral_aresetn]
                     #connect_bd_net -net [get_bd_nets axi_adrv9009_rx_clkgen] [get_bd_pins axi_cpu_interconnect/M16_ACLK] [get_bd_pins axi_adrv9009_rx_clkgen/clk_0]
                     #connect_bd_net [get_bd_pins sys_rstgen/interconnect_aresetn] [get_bd_pins axi_cpu_interconnect/M16_ARESETN]
 
                     if {$rxtx == "rx" || $rxtx == "rxtx"} {
-                        connect_bd_net [get_bd_pins axi_cpu_interconnect/M16_ACLK] [get_bd_pins axi_adrv9009_rx_clkgen/clk_0]
+						connect_bd_net [get_bd_pins axi_cpu_interconnect/aclk1] [get_bd_pins util_adrv9009_xcvr/rx_out_clk_0]
                     }
                     if {$rxtx == "tx" || $rxtx == "rxtx"} {
                         # Remove valid combiner
