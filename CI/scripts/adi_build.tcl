f1e5d93fad4b64c97d5552ed690b85a7a5b8c5d8
Travis F. Collins <travis.collins@analog.com>
2023-05-24 14:17:32 -0600

    Update to HDL bootbin generation and update all support boot files
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- CI/scripts/adi_build.tcl
+++ CI/scripts/adi_build.tcl
@@ -1,20 +1,19 @@
-global fpga_board
-
-if {[info exists fpga_board]} {
-    puts "==========="
-    puts $fpga_board
-    puts "==========="
-} else {
-    # Set to something not ZCU102
-    set fpga_board "ZYNQ"
-}
-
 set prj_carrier $project$carrier
 
-# Define local variables
-set cdir [pwd]
-set sdk_loc vivado_prj.sdk
-set project_system_dir vivado_prj.srcs/sources_1/bd/system
+if {$project == "adrv9361z7035"} {
+    set fpga_board "adrv9361"
+} elseif {$project == "adrv9364z7020"} {
+    set fpga_board "adrv9364"
+} else {
+    set fpga_board_lc [string tolower $fpga_board]
+}
+puts "FPGA Board: $fpga_board_lc"
+
+# Verify support files exist
+if {![file exists $project_dir/projects/common/boot/$fpga_board_lc/u-boot.elf]} {
+    puts "ERROR: Missing u-boot.elf for $fpga_board_lc"
+    return
+}
 
 # Build the project
 update_compile_order -fileset sources_1
@@ -27,6 +26,11 @@ wait_on_run synth_1
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
 
+# Define local variables
+set cdir [pwd]
+set sdk_loc vivado_prj.sdk
+set project_system_dir vivado_prj.srcs/sources_1/bd/system
+
 # Export the hdf
 file delete -force $sdk_loc
 file mkdir $sdk_loc
@@ -37,26 +41,13 @@ close_project
 
 # Create the BOOT.bin
 file mkdir $cdir/boot
-if {$fpga_board eq "ZCU102"} {
-    set vversion [version -short]
-    exec xsct $cdir/projects/scripts/fsbl_build_zynqmp.tcl $vversion
-    if {[file exist boot/BOOT.BIN] eq 0} {
-        puts "ERROR: BOOT.BIN not built"
-        return -code error 11
-    } else {
-        puts "BOOT.BIN built correctly!"
-    }
+set xsct_script "exec xsct $cdir/projects/scripts/adi_make_boot_bin.tcl"
+set arm_tr_frm_elf $cdir/projects/common/boot/bl31.elf
+set uboot_elf $cdir/projects/common/boot/$fpga_board_lc/u-boot.elf
 
-} else {
-    exec xsct $cdir/projects/scripts/fsbl_build_zynq.tcl
-    if {[file exist boot/BOOT.BIN] eq 0} {
-        puts "ERROR: BOOT.BIN not built"
-        return -code error 11
-    } else {
-        puts "BOOT.BIN built correctly!"
-    }
-}
-
+set build_args "$sdk_loc/system_top.xsa $uboot_elf $cdir/boot $arm_tr_frm_elf"
+puts "Please wait, this may take a few minutes."
+eval $xsct_script $build_args
 puts "------------------------------------"
 puts "Embedded system build completed."
 puts "You may close this shell."
