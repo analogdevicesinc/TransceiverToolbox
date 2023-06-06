4a684bca77b88110eb99a54658570970a403b7eb
Pagadarai <Srikanth.Pagadarai@analog.com>
2023-06-06 11:39:19 -0400

    Updated bash script to build LTEPA app
    Signed-off-by: Pagadarai <Srikanth.Pagadarai@analog.com>

--- trx_examples/streaming/LTE_PA_App/genInstaller.sh
+++ trx_examples/streaming/LTE_PA_App/genInstaller.sh
@@ -2,19 +2,12 @@
 screen -S Installer -dm bash -c "$1/$2/bin/matlab $3 -r 'genLTEPAInstaller'"
 echo "MATLAB launched"
 
-for ii in {1..500}
-do
-        echo $ii
-        echo "Active MATLAB session IDs: $(pidof MATLAB)"
-        screen -list | grep Installer
-        if [ -f LTEPA/for_redistribution/LTEPAInstaller.install ]; then
-                echo "Generated installer found"
-                echo "Stopping MATLAB"
-                sleep 4
-                screen -X -S Installer quit
-                screen -list
-                exit 0
-        fi
-        sleep 1
-done
-exit 1
+echo "Active MATLAB session ID: $(pidof MATLAB)"
+screen -list | grep Installer
+if [ -f LTEPAinstaller/LTEPAInstaller ]; then
+	echo "Generated installer found"
+	echo "Stopping MATLAB"
+	screen -X -S Installer quit
+	screen -list
+	exit 0
+fi
