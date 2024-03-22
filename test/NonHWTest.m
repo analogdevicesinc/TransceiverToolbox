e59b9e7b0afdb049bec09fdbd2d8b744c97c894e
Pagadarai <Srikanth.Pagadarai@analog.com>
2024-03-22 13:57:53 -0400

    Removed spaces. Commit to prevent already-tested CI HDL tests from triggering.
    CI: skip=HDL Tests; skip=Demo Tests; skip=Hardware Streaming Tests
    Signed-off-by: Pagadarai <Srikanth.Pagadarai@analog.com>

--- test/NonHWTest.m
+++ test/NonHWTest.m
@@ -1,17 +1,12 @@
 classdef NonHWTest < matlab.unittest.TestCase
-    
     properties(TestParameter)
         rootClasses = getClasses();
     end
         
     methods (Test)
-        
         function call_constructors(testCase,rootClasses)
             sdr = eval(['adi.',rootClasses,'()']);
             testCase.assertEqual(class(sdr),['adi.',rootClasses]);
         end
-        
     end
-    
-    
 end
