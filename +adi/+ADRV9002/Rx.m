d9b1667cea09e9ecf9958ee7f43ca20c8b918dcf
Pagadarai <Srikanth.Pagadarai@analog.com>
2024-10-09 13:59:54 -0500

    ADRV9002: Added properties to access Rx interface gains available
    Signed-off-by: Pagadarai <Srikanth.Pagadarai@analog.com>

--- +adi/+ADRV9002/Rx.m
+++ +adi/+ADRV9002/Rx.m
@@ -200,7 +200,7 @@ classdef Rx < adi.ADRV9002.Base & adi.common.Rx
         %   hardware
         RSSIChannel1
     end
-        
+
     properties(Constant, Hidden)
         ENSMModeChannel0Set = matlab.system.StringSet({ ...
             'calibrated','primed','rf_enabled'});
@@ -221,15 +221,13 @@ classdef Rx < adi.ADRV9002.Base & adi.common.Rx
             'automatic','spi'});
         DigitalGainControlModeChannel1Set = matlab.system.StringSet({ ...
             'automatic','spi'});
-        
-        InterfaceGainChannel0Set = matlab.system.StringSet({...
-            '18dB', '12dB', '6dB', '0dB', '-6dB', '-12dB', '-18dB',...
-            '-24dB', '-30dB','-36dB'});
-        InterfaceGainChannel1Set = matlab.system.StringSet({...
-            '18dB', '12dB', '6dB', '0dB', '-6dB', '-12dB', '-18dB',...
-            '-24dB', '-30dB','-36dB'});
     end
     
+    properties(Hidden)
+        InterfaceGainAvailableChannel0
+        InterfaceGainAvailableChannel1
+    end
+
     properties (Hidden, Nontunable, Access = protected)
         isOutput = false;
     end
@@ -262,6 +260,22 @@ classdef Rx < adi.ADRV9002.Base & adi.common.Rx
                 value = obj.getAttributeDouble('voltage1','rssi',false);
             else
                 value = NaN;
+            end
+        end
+        function values = get.InterfaceGainAvailableChannel0(obj)
+            if obj.ConnectedToDevice
+                values = obj.getAttributeRAW('voltage0','interface_gain_available',false);
+                values = strsplit(values);
+            else
+                values = NaN;
+            end
+        end
+        function values = get.InterfaceGainAvailableChannel1(obj)
+            if obj.ConnectedToDevice
+                values = obj.getAttributeRAW('voltage0','interface_gain_available',false);
+                values = strsplit(values);
+            else
+                values = NaN;
             end
         end
         
@@ -352,6 +366,7 @@ classdef Rx < adi.ADRV9002.Base & adi.common.Rx
         
         % Check InterfaceGainChannel0
         function set.InterfaceGainChannel0(obj, value)
+            mustBeMember(value,obj.InterfaceGainAvailableChannel0);
             obj.InterfaceGainChannel0 = value;
             if obj.ConnectedToDevice
                 id = 'voltage0';
@@ -363,6 +378,7 @@ classdef Rx < adi.ADRV9002.Base & adi.common.Rx
         end
         % Check InterfaceGainChannel1
         function set.InterfaceGainChannel1(obj, value)
+            mustBeMember(value,obj.InterfaceGainAvailableChannel1);
             obj.InterfaceGainChannel1 = value;
             if obj.ConnectedToDevice
                 id = 'voltage1';
--- test/ADRV9002Tests.m
+++ test/ADRV9002Tests.m
@@ -321,7 +321,7 @@ classdef ADRV9002Tests < HardwareTests
             rx.CustomStreamFileName = which('lte_5_cmos_api_68_0_6.stream');
 
             rx.DigitalGainControlModeChannel0 = 'spi';
-            rx.InterfaceGainChannel0 = '6dB';
+            rx.InterfaceGainChannel0 = rx.InterfaceGainAvailableChannel0{1};
             verifyError(testCase, @() rx(), ?MException);
         end
 
@@ -335,7 +335,7 @@ classdef ADRV9002Tests < HardwareTests
             rx.CustomStreamFileName = which('lte_5_cmos_api_68_0_6.stream');
 
             rx.DigitalGainControlModeChannel0 = 'spi';
-            rx.InterfaceGainChannel0 = '6dB';
+            rx.InterfaceGainChannel0 = rx.InterfaceGainAvailableChannel0{1};
             [~,valid] = rx();
             testCase.assertTrue(valid);
         end
