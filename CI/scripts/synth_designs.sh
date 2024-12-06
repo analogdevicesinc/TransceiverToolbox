fff08940f12a5fc4cba334cf88a82152bf4705a3
Travis F. Collins <travis.collins@analog.com>
2024-12-06 16:57:03 -0700

    Randomize xvfb ID for synth jobs
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- CI/scripts/synth_designs.sh
+++ CI/scripts/synth_designs.sh
@@ -14,8 +14,10 @@ cd ../..
 cp hdl/vendor/AnalogDevices/hdlcoder_board_customization.m test/hdlcoder_board_customization_local.m
 sed -i "s/hdlcoder_board_customization/hdlcoder_board_customization_local/g" test/hdlcoder_board_customization_local.m
 source /opt/Xilinx/Vivado/2022.2/settings64.sh
-Xvfb :77 &
-export DISPLAY=:77
+# Randomize DISPLAY number to avoid conflicts
+export DISPLAY_ID=:$(shuf -i 10-1000 -n 1)
+Xvfb :$DISPLAY_ID &
+export DISPLAY=:$DISPLAY_ID
 export SWT_GTK3=0
 source /opt/Xilinx/Vivado/2022.2/settings64.sh
 $MLPATH/$MLRELEASE/bin/matlab $MLFLAGS -r "cd('test');runSynthTests('$BOARD');"
